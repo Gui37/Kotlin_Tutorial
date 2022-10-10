35a66ec90ad13b907ad110633b2828000797e48b
Gui37 <guifty32@gmail.com>
2022-10-10 18:29:24 +0200

    Simple Variables tests

--- src/main/kotlin/HWorld.kt
+++ src/main/kotlin/HWorld.kt
@@ -1,7 +1,8 @@
-fun main (){
+fun main () {
     println("Hello, World")
-    teste()
-}
-fun teste(){
-    println("Hello, Kotlin")
+    var userName: String="Dante"
+    val age: Int=23
+//    var usa-se para variaveis mutaveis e val usa-se para variaveis imutaveis
+    println("Hello there, my name is $userName i'm $age years old and in 4 years i'll have ${age +4}")
+//   o simbolo $ e um placeholder, onde coloca-se variaveis dentro de strings
 }
