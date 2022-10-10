dfa1bd87789463e9f0a701562b0474558ab4892d
Gui37 <guifty32@gmail.com>
2022-10-10 21:08:29 +0200

    For Loops and Range tests

--- src/main/kotlin/HWorld.kt
+++ src/main/kotlin/HWorld.kt
@@ -1,14 +1,25 @@
 fun main () {
   val names = arrayOf("Test1","Test2","Test3", "Test4","Test5")
-    println("The size of this array is ${names.size}")
-    println("The first test is : ${names[0]}")
-    names [0]= "Test"
-    println("The first test is : ${names[0]}")
-    println("The last element of this array is ${names[names.size-1]}")
-    println("-------------------------------------------------------")
+    for(name in names){
+      println(name)
+    }
+    for (name in names.indices){
+      println("index = $name Element=${names[name]}")
+
+    }
+
     val numbers = intArrayOf(1,2,3,4,5,6,7,8,9)
-    println("The size of this array is :${numbers.size}")
-    println("The lenght of this array is :${numbers.size-1}")
-    println("The last element of this array is ${numbers[numbers.size-1]}")
-    println("The sum of the first element and last element is :${numbers[numbers.size-1]+numbers[0]}")
+      for (number in numbers){
+        print("$number ")
+      }
+    for(i in 1..10){//for (i in 1 until 10) nao chega 10
+      println(i)
+    }
+    for (i in 10 downTo 0 step 2){
+      println(i)
+    }
+    for (i in 'a'..'z'){
+      println(i)
+    }
+
 }
