c537d8a20e9c2efeb1ffce7d5247324501fad2d7
Gui37 <guifty32@gmail.com>
2022-10-11 16:21:08 +0200

    Logic operators and comparison tests

--- src/main/kotlin/HWorld.kt
+++ src/main/kotlin/HWorld.kt
@@ -1,25 +1,11 @@
 fun main () {
-  val names = arrayOf("Test1","Test2","Test3", "Test4","Test5")
-    for(name in names){
-      println(name)
-    }
-    for (name in names.indices){
-      println("index = $name Element=${names[name]}")
-
-    }
-
-    val numbers = intArrayOf(1,2,3,4,5,6,7,8,9)
-      for (number in numbers){
-        print("$number ")
-      }
-    for(i in 1..10){//for (i in 1 until 10) nao chega 10
-      println(i)
-    }
-    for (i in 10 downTo 0 step 2){
-      println(i)
-    }
-    for (i in 'a'..'z'){
-      println(i)
-    }
+  val a =50
+  println("2 > 1 is ${2>1}")
+  println("2 < 1 is ${2<1}")
+  println("2 = 1 is ${2==1}")
+  println("2 = 1 is ${2!=1}")
+  println("!(2 > 1) is ${!(2>1)}")
+  println("Operator and (true && false) ${true && false}")
+  println("Operator Or (true && false) ${true || false}")
 
 }
