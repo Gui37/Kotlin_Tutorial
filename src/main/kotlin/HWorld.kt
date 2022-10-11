2dee4cd55404720b8b337992f2151c303a10ce74
Gui37 <guifty32@gmail.com>
2022-10-12 01:39:33 +0200

    Do While and Labels tests

--- src/main/kotlin/HWorld.kt
+++ src/main/kotlin/HWorld.kt
@@ -1,11 +1,64 @@
 fun main () {
-  val a =50
-  println("2 > 1 is ${2>1}")
-  println("2 < 1 is ${2<1}")
-  println("2 = 1 is ${2==1}")
-  println("2 = 1 is ${2!=1}")
-  println("!(2 > 1) is ${!(2>1)}")
-  println("Operator and (true && false) ${true && false}")
-  println("Operator Or (true && false) ${true || false}")
+  var i = 0
+  var a =5
+  do{
+    println(a)//ou println(i++)
+    a++
+  } while (a<5)
+println("*----------------------------*")
+  while (i<5){
+    println(i)//ou println(i++)
+    i++
+  }
+  println("*----------------------------*")
+  do{
+    println(i)//ou println(i++)
+    i++
+
+    var j =0
+    while (j<5){
+      println("-----$j")
+      j++
+      break
+      print("I'm not printed")
+    }
+    println("I am printed")
+  } while (i<9)
+  println("*----------------------------*")
+  do{
+    println(i)//ou println(i++)
+    i++
+    continue
+    var j =0
+    while (j<5){
+      println("-----$j")
+      j++
+      break
+      print("I'm not printed")
+    }
+    println("I am printed")
+  } while (i<9)
+  println("*----------------------------*")
+  outer@do{
+    println(i)//ou println(i++)
+    i++
+    var j =0
+    while (j<5){
+      println("-----$j")
+      j++
+      break@outer
+      print("I'm not printed")
+    }
+    println("I am printed")
+  } while (i<10)
+
+  println("*----------------------------*")
+  var x = 100
+  var cont=0
+  while (x>1) {
+      x = x / 2
+      cont++
+  }
+  println(cont)
 
 }
