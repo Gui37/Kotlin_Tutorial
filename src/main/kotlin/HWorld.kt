ae9a26e72da1c39f3fddb179d825c79b707c40d5
Gui37 <guifty32@gmail.com>
2022-10-12 02:54:47 +0200

    If Else tests

--- src/main/kotlin/HWorld.kt
+++ src/main/kotlin/HWorld.kt
@@ -1,64 +1,32 @@
 fun main () {
-  var i = 0
-  var a =5
-  do{
-    println(a)//ou println(i++)
-    a++
-  } while (a<5)
-println("*----------------------------*")
-  while (i<5){
-    println(i)//ou println(i++)
-    i++
+  for (i in 1..10){
+    if (i == 3) continue//ou if (i in 4..7) continue// ou if (i !in 4..7) continue
+    println(i)
   }
-  println("*----------------------------*")
-  do{
-    println(i)//ou println(i++)
-    i++
 
-    var j =0
-    while (j<5){
-      println("-----$j")
-      j++
-      break
-      print("I'm not printed")
-    }
-    println("I am printed")
-  } while (i<9)
-  println("*----------------------------*")
-  do{
-    println(i)//ou println(i++)
-    i++
-    continue
-    var j =0
-    while (j<5){
-      println("-----$j")
-      j++
-      break
-      print("I'm not printed")
-    }
-    println("I am printed")
-  } while (i<9)
-  println("*----------------------------*")
-  outer@do{
-    println(i)//ou println(i++)
-    i++
-    var j =0
-    while (j<5){
-      println("-----$j")
-      j++
-      break@outer
-      print("I'm not printed")
-    }
-    println("I am printed")
-  } while (i<10)
-
-  println("*----------------------------*")
-  var x = 100
-  var cont=0
-  while (x>1) {
-      x = x / 2
-      cont++
+  for (i in 1..10){
+    if (i !in 4..7) print('-') else print('+')
+    println(i)
   }
-  println(cont)
-
+  for (i in 1..10){
+    val prefix =if(i==1){
+      '*'
+    }else if (i !in 4..7) {
+      print('-')
+      '>'
+    } else {
+      '+'
+    }
+    println("$prefix$i")
+  }
+  val a = 9
+    if (a in 0..11){
+      println("Good Morning")
+    }else if (a==12){
+      println("Time for Lunch")
+    }else{
+      println("Time for Rest Boy")
+    }
 }
+
+
