da099def2a3f56a7de79cd59607565b76a7d0b4f
Gui37 <guifty32@gmail.com>
2022-10-18 16:51:19 +0200

    default and named Function tests

--- src/main/kotlin/HWorld.kt
+++ src/main/kotlin/HWorld.kt
@@ -1,29 +1,10 @@
 fun main () {
-    println(getMax(20,30))
-    println(getMax(20.5,30.63))
-    println(getMax(2654519854540,3065464561))
-    println(getMax(21.23120,30123.12312))
-    println(getMax(220,330,40))
-
+    println(greet("How your doing?","Dnt"))
 }
-//This is called function overloading
-//we can use it when we have multiple cases with different types
 
-
-fun getMax(a: Int, b: Int)= if (a>b) a else b
-
-fun getMax(a: Double, b: Double)= if (a>b) a else b
-
-fun getMax(a: Long, b: Long)= if (a>b) a else b
-
-fun getMax(a: Float, b: Float)= if (a>b) a else b
-
-//with function overloading we can change the number of arguments, but not the names of it
-fun getMax(a: Int, b: Int, c:Int)=
-    when{
-        a>=b && a>=c ->a
-        b>=a && b>=c ->b
-        else -> c
+fun greet(msg:String, name:String="Gui", rep:Int = 2) {//This is called a default argument
+    for (i in 0 until rep) {
+        println("Hello There! Your name is $name right? $msg")
     }
-
+}
 
