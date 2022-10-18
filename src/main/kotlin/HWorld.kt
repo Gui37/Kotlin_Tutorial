2e19a782728258d321f07a8766d7240166d84824
Gui37 <guifty32@gmail.com>
2022-10-18 15:22:15 +0200

    Single Expression Function tests

--- src/main/kotlin/HWorld.kt
+++ src/main/kotlin/HWorld.kt
@@ -1,30 +1,9 @@
 fun main () {
-    var maxV = getMax(10,30)
-    println(maxV)//we could call the function inside the println function
-    maxV = getMax2(50,30)
-    println(maxV)//we could call the function inside the println function
     println(double(10))
+    println(getMax(20,30))
 }
 
-fun getMax(numb1: Int, numb2: Int): Int{
-    val max: Int = if (numb1>numb2){
-        numb1
-    }else
-        numb2
-    return max
-}
+fun double(num: Int): Int= num*2// This is called a single expression function
 
-fun getMax2(numb3: Int, numb4:Int): Int{
-    if (numb3>numb4){
-        return numb3
-    } else {
-        return numb4
-    }
-
-}
-
-fun double(num: Int): Int{
-    return num*2
-}
-
+fun getMax(a: Int, b: Int): Int= if (a>b) a else b
 
