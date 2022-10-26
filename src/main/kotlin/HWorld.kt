9d2d202091e71dff769cd6d4aaeb6bdba82da5ad
Gui37 <guifty32@gmail.com>
2022-10-26 03:49:47 +0200

    init blocks tests

--- src/main/kotlin/HWorld.kt
+++ src/main/kotlin/HWorld.kt
@@ -1,20 +1,21 @@
-fun main() { //we have different ways of implement a class in kotlin, like the way we define it inside the class
-    //with this one, we define the values in the class, not in the instance of it
-    val person = Person()
-    println("Person One\nPerson name: ${person.name} \nPerson age: ${person.age} \nPerson height: ${person.height}")
-
-    //Here we define the values in the instance of the class
-    val persona= Person2(name="Hobbo",age=35,height=180)
-    println("Person One\nPerson name: ${persona.name} \nPerson age: ${persona.age} \nPerson height: ${persona.height}")
-
+fun main() {
+    val person1 =Person("Dante",25,170)
+    val person2 =Person("",25,170)
+    val person3 =Person("Jane",29,150)
+    val person4 =Person("Dee",35,130)
 }
+class Person(name: String, var age: Int, var height: Int){
+    init {
+        println("New Person created.")
+    }
 
-class Person{
-    var name="Gui"
-    var age=27
-    var height=170
-}
-
-class Person2(name: String, var age: Int, var height: Int){//it's called a constructor, and we call it in the instance of the class
-    var name=name // inside a class we call it a property instead of variables
+    val name2:String
+    init {
+        if (name.isBlank()) {
+            this.name2 = "John Doe or Jane Doe"
+        }else{
+            this.name2=name.trim()
+        }
+        println("Name: ${this.name2} Age:$age  Height:$$height")
+    }
 }
